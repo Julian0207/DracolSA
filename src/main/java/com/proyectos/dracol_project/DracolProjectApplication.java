4ddea0d6c9007e34a02cad7750e3ccf9a4133aa5
lau05avend <lvavendano6@misena.edu.co>
2021-06-03 15:49:25 -0500

    Primer version de proyecto Dracol.

---
+++ src/main/java/com/proyectos/dracol_project/DracolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.proyectos.dracol_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DracolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectos/dracol_project/DracolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectos.dracol_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DracolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
